f593deab7b85a7fe97ca8650e8fdfc98d49031a7
colinlmcdonald <mickydwow@gmail.com>
2016-02-05 17:04:18 -0800

    Passed the specs!! WOOO HOOOO. Solved by staring at the fountain.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,7 +9,7 @@ var app = {
 
     $('#send').submit(function(event) {
       app.handleSubmit();
-      event.preventDefault();
+      event.stopImmediatePropagation();
     });
 
   },
@@ -22,7 +22,6 @@ var app = {
     };
 
     $.ajax({
-      // This is the url you should use to communicate with the parse API server.
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'POST',
       data: JSON.stringify(message),
@@ -31,7 +30,6 @@ var app = {
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message');
       }
     });
@@ -40,14 +38,12 @@ var app = {
 
   fetch: function() {
     $.ajax({
-      // This is the url you should use to communicate with the parse API server.
       url: undefined,
       type: 'GET',
       success: function (data) {
         console.log('chatterbox: Message received');
       },
       error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to receive message');
       }
     });
@@ -61,7 +57,6 @@ var app = {
     var $test = $("<div><a href='#' class='username'>" +
       message.username +
       "</a></div>");
-    //$test.
 
     $('#chats').append($test);
   },  
@@ -75,24 +70,13 @@ var app = {
   },
 
   handleSubmit: function() {
-    alert('you rang?');
+
   }
 
 };
 
 
-$( function() {
-    
-      // app.addMessage({
-      //   username: 'Mel Brooks',
-      //   text: 'I didn\'t get a harumph outa that guy.!',
-      //   roomname: 'lobby'
-      // });
-  $('#message').val('Why so many Mel Brooks quotes?');
 
-  app.init();
-      
-  });
 
 
 
